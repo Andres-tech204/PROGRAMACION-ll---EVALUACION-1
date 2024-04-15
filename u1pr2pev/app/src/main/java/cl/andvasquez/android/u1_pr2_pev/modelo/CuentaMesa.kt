aa2e7c5e9ac5ff38d6814e9717bf886cb37bb0e1
Andres-tech204 <154020982+Andres-tech204@users.noreply.github.com>
2024-04-15 15:32:33 -0400

    Update CuentaMesa.kt

--- u1pr2pev/app/src/main/java/cl/andvasquez/android/u1_pr2_pev/modelo/CuentaMesa.kt
+++ u1pr2pev/app/src/main/java/cl/andvasquez/android/u1_pr2_pev/modelo/CuentaMesa.kt
@@ -13,21 +13,6 @@ class CuentaMesa(val numeroMesa: Int) {
         items.add(itemMesa)
     }
 
-    fun calcularTotalSinPropina(): Int {
-        var total = 0
-        for (item in items) {
-            total += item.calcularSubtotal()
-        }
-        return total
-    }
-
-    fun calcularPropina(): Int {
-        return (calcularTotalSinPropina() * 0.1).toInt()
-    }
-
-    fun calcularTotalConPropina(): Int {
-        return calcularTotalSinPropina() + if (aceptaPropina) calcularPropina() else 0
-    }
 }
 class ItemMesa(val itemMenu: ItemMenu, var cantidad: Int) {
     fun calcularSubtotal(): Int {
